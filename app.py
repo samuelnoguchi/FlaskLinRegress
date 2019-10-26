c9370549c5ba62f4647595664441f9f40ac3baab
samuelnoguchi <33236743+samuelnoguchi@users.noreply.github.com>
2019-10-25 22:00:27 -0400

    predictor works

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import numpy as numpy
+import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
@@ -9,3 +9,27 @@ model = pickle.load(open('model.pkl', 'rb'))
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST']) 
+def predict():
+    # for rendering results on html gui
+    int_features = [int(x) for x in request.form.values()]  
+    final_features = [np.array((int_features))]
+    prediction =  model.predict(final_features)
+
+    output = round(prediction[0], 2)
+
+    return render_template('index.html', prediction_text = 'Employee Salary should be $ {}'. format(output))
+
+@app.route('/predict_api', methods=['POST'])
+def predict_api():
+    # for direct API calls through request
+
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify((output))
+
+if __name__ == "__main__":
+    app.run(debug=True)
