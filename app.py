15490e8e5cac3db6770553ef519f14e6cfe94b8b
samuelnoguchi <33236743+samuelnoguchi@users.noreply.github.com>
2019-10-25 21:27:33 -0400

    Initial Commit

---
+++ app.py
@@ -0,0 +1,8 @@
+import numpy as numpy
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+#initialize flask app and load model
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
